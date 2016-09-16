4be6ca05549f3ef4d85efa252bac35732ac3256c
TimT-code <ttesner+twitter@gmail.com>
2016-09-15 21:21:07 -0400

    Update stylZ.js

--- stylZ.js
+++ stylZ.js
@@ -1,33 +1,42 @@
 var Z_=function(el){/* Z_ is an alias for "styleZ" */
-  var el=document.getElementById(el);
-  var elz=el.style;
-  
-  var zss=function(property,value){
-    elz[property]=value;
-    return this;
-  };  
-  
-  var z_c=function(colorz){
-    elz.color=colorz;
-    return this;
+  var/*comma chain*/
+    el=document.getElementById(el),
+    elz=el.style,
+      
+    zss=function(property,value){
+      elz[property]=value;
+      return this;
+    },
+      
+    z_c=function(colorz){
+      elz.color=colorz;
+      return this;
+    },
+      
+    z_bgc=function(bg_colorz){
+      elz.backgroundColor=bg_colorz;
+      return this;
+    },
+      
+    z_b=function(value){
+      elz.border=value;
+      return this;
+  },
+      
+    z_w=function(value){
+      elz.width=value;
+      return this;
   };
   
-  var z_bgc=function(bg_colorz){
-    elz.backgroundColor=bg_colorz;
-    return this;
+  return {
+    el:el,
+    elz:elz,
+    zss:zss,
+    z_c:z_c,
+    z_bgc:z_bgc,
+    z_b:z_b,
+    z_w:z_w
   };
-  
-  var z_b=function(value){
-    elz.border=value;
-    return this;
-  };
-  
-  var z_w=function(value){
-    elz.width=value;
-    return this;
-  }
-  
-  return {el:el,elz:elz,zss:zss,z_c:z_c,z_bgc:z_bgc,z_b:z_b,z_w:z_w};
 };
 
 //test code - compare length of code - The id's and color's name's characters are same length to compare
