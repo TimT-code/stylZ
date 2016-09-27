665e49cf31c5e5ceda3c493818e8d963668f0b40
TimT-code <ttesner+twitter@gmail.com>
2016-09-27 18:29:28 -0400

    Update stylZ.js

--- stylZ.js
+++ stylZ.js
@@ -1,6 +1,6 @@
 var stylZ=(function(){
     var stylZ=function(el){/* 'Z_' is an alias for 'styleZ' */
-      var
+      var/*comma chain*/
         el=document.getElementById(el),
         elz=el.style,
 
@@ -36,9 +36,7 @@ var stylZ=(function(){
         z_bt=function(val){elz.borderTop=val;return this;},          
         z_br=function(val){elz.borderRight=val;return this;},          
         z_bb=function(val){elz.borderBottom=val;return this;},          
-        z_bl=function(val){elz.borderLeft=val;return this;},          
-        z_bcl=function(val){elz.borderCollapse=val;/*separate|collapse|initial|inherit*/return this;},          
-        z_bsp=function(val){elz.borderSpacing=val;/*has no effect if borderCollapse is set to 'collapse'*/return this;},
+        z_bl=function(val){elz.borderLeft=val;return this;},        
           
         z_bc=function(val){elz.borderColor=val;/*color|transparent|initial|inherit - up to 4 values*/return this;},        
         z_btc=function(val){elz.borderTopColor=val;return this;},          
@@ -57,6 +55,9 @@ var stylZ=(function(){
         z_brw=function(val){elz.borderRightWidth=val;return this;},          
         z_bbw=function(val){elz.borderBottomWidth=val;return this;},          
         z_blw=function(val){elz.borderLeftWidth=val;return this;},
+          
+        z_bcl=function(val){elz.borderCollapse=val;/*separate|collapse|initial|inherit*/return this;},          
+        z_bsp=function(val){elz.borderSpacing=val;/*has no effect if borderCollapse is set to 'collapse'*/return this;},
           
         z_p=function(val){elz.padding=val;return this;},          
         z_pt=function(val){elz.paddingTop=val;return this;},          
@@ -77,6 +78,7 @@ var stylZ=(function(){
         z_wsp=function(val){elz.wordSpacing=val;return this;},/*normal|length|initial|inherit - number can be positive, or negative*/
           
         z_w=function(val){elz.width=val;return this;};/*auto|length|%|initial|inherit*/
+      
         z_h=function(val){elz.height=val;return this;};/*auto|length|%|initial|inherit*/
       
         z_zi=function(val){elz.height=val;return this;}/*auto|number|initial|inherit - number can be positive, or negative*/
@@ -101,9 +103,7 @@ var stylZ=(function(){
         z_bt:z_bt,
         z_br:z_br,
         z_bb:z_bb,
-        z_bl:z_bl,
-        z_bcl:z_bcl,
-        z_bsp:z_bsp,
+        z_bl:z_bl,        
         z_bc:z_bc,
         z_btc:z_btc,
         z_brc:z_brc,
@@ -119,6 +119,8 @@ var stylZ=(function(){
         z_brw:z_brw,
         z_bbw:z_bbw,
         z_blw:z_blw,
+        z_bcl:z_bcl,
+        z_bsp:z_bsp,
         
         z_p:z_p,
         z_pt:z_pt,
